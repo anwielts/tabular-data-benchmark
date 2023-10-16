d239aa791aed617dc809990d797db496f8f43f8d
ACW <andreaswieltsch@gmail.com>
2023-10-16 21:42:32 +0200

    Extended DataLoader class making it more variable

--- data/DataLoader.py
+++ data/DataLoader.py
@@ -5,14 +5,27 @@ from sklearn.datasets import fetch_openml
 
 class TabularDataLoader:
     def __init__(self,
-                 feature_target_splitting=True,
+                 datasets: list,
+                 output_format='pandas',
+                 feature_target_splitting: bool =True,
                  ):
         self.feature_target_splitting = feature_target_splitting
+        self.datasets = datasets
+        self.output_format = output_format
 
-        self.features = None
-        self.targets = None
+        self.features = []
+        self.targets = []
 
-    def load_tabular_data(self):
-        self.features, self.targets = fetch_openml(
-            "titanic", version=1, as_frame=True, return_X_y=True, parser="pandas"
+        for dataset in datasets:
+            x, y = self.load_tabular_data(dataset)
+            self.features.append(x)
+            self.targets.append(y)
+
+    def load_tabular_data(self, dataset_name: str):
+        features, targets = fetch_openml(
+            dataset_name, version=1, as_frame=True, 
+            return_X_y=self.feature_target_splitting, 
+            parser=self.output_format
         )
+
+        return features, targets
